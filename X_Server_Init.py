a422b96f7128976e870bbaf254e5b9a76000a622
taha <taha.m.gokdemir@gmail.com>
2020-11-29 12:56:16 +0300

    null

--- X_Server_Init.py
+++ X_Server_Init.py
@@ -6,4 +6,10 @@ def initXserver():
     global XServerProcess
     XServerProcess = subprocess.Popen(shlex.split('sudo /usr/bin/X :0'), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
+def isXserverRunning():
+    p = subprocess.Popen(["xset", "-q"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.communicate()
+    print(p.returncode)
+    return p.returncode == 0
+
 
