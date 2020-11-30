7eaddc8f5891b0e83dbd10eaac69eafa745aa7d4
taha <taha.m.gokdemir@gmail.com>
2020-11-30 04:43:40 +0300

    null

--- instance_initializer.py
+++ instance_initializer.py
@@ -1,4 +1,4 @@
-import asyncio, x_server_utils, aws_utils, vpn_server_utils
+import threading, time, x_server_utils, aws_utils, vpn_server_utils
 import config, simulair_core_utils
 
 SimulationInfo = None
@@ -27,7 +27,7 @@ def setInstanceStatus(status):
         aws_utils.setStatus(SimulationInfo["_id"], status)
 
 
-async def run_x_server(timeout=20, delay=3):
+def run_x_server(timeout=20, delay=2):
     loop_count = timeout // delay
     for i in range(0, loop_count):
         if not x_server_utils.isXserverRunning():
@@ -35,32 +35,27 @@ async def run_x_server(timeout=20, delay=3):
         else:
             x_server_utils.activateDisplay()
             return True
-        return await asyncio.sleep(delay)
+        time.sleep(delay)
     result["status"] = 400
     result["meta"]["message"] = "X server initialization failed";
     return False
 
-async def run_demo_sim(socketIP):
-    await asyncio.sleep(2)
+def run_demo_sim(socketIP):
+    time.sleep(2)
     simulair_core_utils.initCoreProcess(config.DEMO_CORE_PATH, socketIP)
     setInstanceStatus("pending3")
-    return await asyncio.sleep(10)
+    time.sleep(3)
 
-
-
-
-async def async_initialize():
+def async_initialize():
     if SimulationInfo is not None:
         vpn_server_utils.initVpnServer(SimulationInfo["instance_info"]["publicIpAddress"], SimulationInfo["instance_info"]["privateIpAddress"])
-        await asyncio.sleep(5)
-        await run_x_server()
-        return await  run_demo_sim(SimulationInfo["instance_info"]["publicIpAddress"])
+        time.sleep(3)
+        run_x_server()
+        run_demo_sim(SimulationInfo["instance_info"]["publicIpAddress"])
 
 
 def initialize():
-    getInstanceInfo()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_initialize())
-    loop.close()
+    x = threading.Thread(target=async_initialize())
+    x.start()
     return result
 
--- simulair_core_utils.py
+++ simulair_core_utils.py
@@ -4,6 +4,5 @@ CoreProcess= None
 
 def initCoreProcess(env_id, socketIP):
     global CoreProcess
-    CoreProcess = subprocess.Popen(shlex.split('sudo python3 {}/start_player.py socketPort={} socketIP={}'.format(config.CORE_PATH+"/"+env_id, config.MANAGER_PORT, socketIP)),
-                                   stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    CoreProcess = subprocess.Popen(' export DISPLAY=:0 && sudo python3 {}/start_player.py socketPort={} socketIP={}'.format(config.CORE_PATH+"/"+env_id, config.MANAGER_PORT, socketIP), shell=True)
 
