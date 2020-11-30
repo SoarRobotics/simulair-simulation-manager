274d23eec3dba3a32dca6fe28d6c18fe0b6a002d
taha <taha.m.gokdemir@gmail.com>
2020-11-30 03:58:00 +0300

    null

--- simulair_core_utils.py
+++ simulair_core_utils.py
@@ -6,5 +6,4 @@ def initCoreProcess(env_id, socketIP):
     global CoreProcess
     CoreProcess = subprocess.Popen(shlex.split('sudo python3 {}/start_player.py socketPort={} socketIP={}'.format(config.CORE_PATH+"/"+env_id, config.MANAGER_PORT, socketIP)),
                                    stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = CoreProcess.communicate()
-    print(err)
+
